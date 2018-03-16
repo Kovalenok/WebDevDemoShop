2918a76dcb6bdded8bd0b903be02e84422097def
SD <denisov.univ@gmail.com>
2018-03-17 00:13:11 +0200

    ajax exps v2

--- JsStart/js/generate-catalogue-ajax.js
+++ JsStart/js/generate-catalogue-ajax.js
@@ -1,51 +1,77 @@
-//var obj = JSON.parse('[{"name":"Кавоварка", "image":"img/img1.png", "isSuper":0}]')
+var exchangeRates = null;
 
-var products = [
-    {name: 'Кавоварка', image: 'img/img1.png', isSuper: 0},
-    {name: 'Ноутбук', image: 'img/img2.png', isSuper: 0},
-    {name: 'Курс з веб-розробки', image: 'img/img3.png', isSuper: 0},
-    {name: 'Кавоварка2', image: 'img/img1.png', isSuper: 0},
-    {name: 'Кавоварка3', image: 'img/img2.png', isSuper: 0},
-    {name: 'Super Product', image: 'img/img3.png', isSuper: 1}
-];
+function calculate() {
+
+    var currencySelect = $("#currencyCode");
+
+    var selectedOption = currencySelect.find(":selected").text();
+
+    var currencyCode = selectedOption;//'USD';
+    var currencyInfo = null;
+
+    for(var i=0;i<exchangeRates.length;i++) {
+        if(exchangeRates[i].cc === currencyCode) {
+            currencyInfo = exchangeRates[i];
+            break;
+        }
+    }
+
+    var inputSum = $("#amount").val();
+
+    $("#convertedAmount").val(inputSum / currencyInfo.rate);
+}
+
+function generateBlogBlocks(blogPosts) {
+    for (var i = 0; i < blogPosts.length; i++) {
+        var textBlock = $("<div />");
+        textBlock.addClass('blog-post-text');
+        textBlock.text(blogPosts[i].body);
+
+        var tile = $('<li class="tile"></li>');
+        tile.append('<h1>'+blogPosts[i].title+'</h1>');
+        tile.append(textBlock);
+
+        $('.products-list').append(tile);
+    }
+}
+
+function generateCourses(courses) {
+    var currencySelect = $("#currencyCode");
+
+    for (var i = 0; i < courses.length; i++) {
+        var textBlock = $("<div />");
+        textBlock.text(courses[i].rate);
+
+        var tile = $('<li class="tile"></li>');
+        tile.append('<h1>'+courses[i].txt + '(' + courses[i].cc + ')' +'</h1>');
+        tile.append(textBlock);
+
+        $('.products-list').append(tile);
+
+        currencySelect.append($("<option value='"+courses[i].cc+"'>"+courses[i].cc+"</option>"));
+    }
+}
 
 $(document).ready(
     function() {
         console.log('Document loaded!');
-
-        // jquery зі звичайним циклом
-        for (var i = 0; i < products.length; i++) {
-            var img = $("<img />");
-            // jQuery-обгортка навколо <img />
-
-            img.attr('src', products[i].image);
-            img.attr('alt', products[i].name);
-
-            if(products[i].isSuper)
-                img.addClass('super');
-
-            var tile = $('<li class="tile"></li>');
-            tile.append('<h1>'+products[i].name+'</h1>');
-            tile.append(img);
-
-            $('.products-list').append(tile);
-        }
-
-        // варіант з перебором через jQuery
-        $.each(products, function( index, value ) {
-            var img = $("<img />");
-
-            img.attr('src', value.image);
-            img.attr('alt', value.name);
-
-            if(value.isSuper)
-                img.addClass('super');
-
-            var tile = $('<li class="tile"></li>');
-            tile.append('<h1>'+value.name+'</h1>');
-            tile.append(img);
-
-            $('.products-list').append(tile);
-        });
-    }
+/*
+        $.get(
+            //"https://www.mocky.io/v2/5aa9556e3200002a2d165c54",
+            "https://jsonplaceholder.typicode.com/posts",
+            function( blogPosts ) {
+                console.log("Server returned:", blogPosts)
+                // Не потрібно, якщо сервер повертає тип application/json
+                // JSON.parse(blogPosts)
+                generateBlogBlocks(blogPosts);
+            });
+*/
+        $.get(
+            "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json",
+            function( courses ) {
+                console.log("Server returned:", courses)
+                exchangeRates = courses;
+                generateCourses(courses);
+            });
+  }
 );
--- JsStart/js/generate-catalogue-jq.js
+++ JsStart/js/generate-catalogue-jq.js
@@ -1,9 +1,12 @@
+//var obj = JSON.parse('[{"name":"Кавоварка", "image":"img/img1.png", "isSuper":0}]')
+
 var products = [
     {name: 'Кавоварка', image: 'img/img1.png', isSuper: 0},
-    {name: 'Ноутбук', image: 'img/img2.png', isSuper: 1},
+    {name: 'Ноутбук', image: 'img/img2.png', isSuper: 0},
     {name: 'Курс з веб-розробки', image: 'img/img3.png', isSuper: 0},
     {name: 'Кавоварка2', image: 'img/img1.png', isSuper: 0},
-    {name: 'Кавоварка3', image: 'img/img2.png', isSuper: 0}
+    {name: 'Кавоварка3', image: 'img/img2.png', isSuper: 0},
+    {name: 'Super Product', image: 'img/img3.png', isSuper: 1}
 ];
 
 $(document).ready(
@@ -21,11 +24,11 @@ $(document).ready(
             if(products[i].isSuper)
                 img.addClass('super');
 
-            var tile = $('<div></div>');
+            var tile = $('<li class="tile"></li>');
             tile.append('<h1>'+products[i].name+'</h1>');
             tile.append(img);
 
-            $('.catalogue').append(tile);
+            $('.products-list').append(tile);
         }
 
         // варіант з перебором через jQuery
@@ -38,7 +41,11 @@ $(document).ready(
             if(value.isSuper)
                 img.addClass('super');
 
-            $('.catalogue').append(img);
+            var tile = $('<li class="tile"></li>');
+            tile.append('<h1>'+value.name+'</h1>');
+            tile.append(img);
+
+            $('.products-list').append(tile);
         });
     }
 );
